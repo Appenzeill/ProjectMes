8b8d15cc59e0ec4b7bd1591a2315c70cbf82a1cc
mrdark2001 <pieterspeksnijder1@gmail.com>
2019-04-17 12:48:37 +0200

    Flexibel maken van de role is bij deze helemaal af

--- Includes/parts/roleEdit.php
+++ Includes/parts/roleEdit.php
@@ -43,7 +43,7 @@ $query = Database::getInstance()->query(
                     <option value=""></option>
 					<?php
 					foreach ($query->results() as $query) {
-						echo "<option value='$query->role_name' "; echo "> ".$query->role_name."</option>";
+						echo "<option " . (isset($_POST["selectRole"]) && $query->role_name == $_POST['selectRole'] ? "selected" : "" ) . " value='$query->role_name' "; echo "> ".$query->role_name."</option>";
 					}
 					?>
                 </select>
@@ -54,17 +54,11 @@ $query = Database::getInstance()->query(
 				if (!$rawpermissions->count()) {
 					echo "<br>Geen rol geselecteerd<br>";
 				} else {
-
-
-
 					$permissions = Database::getInstance()->query(
 						"SELECT  user_permissions.*, filtered_list.id as lists_id, filtered_list.role_name, filtered_list.user_permission_list_id, filtered_list.user_permission_id FROM user_permissions LEFT JOIN (SELECT * FROM user_permission_lists WHERE user_permission_lists.role_name =  '$selectRoleName') filtered_list
                                 ON user_permissions.id = filtered_list.user_permission_id
                                 ORDER BY user_permissions.id;");
-
-
 					echo "<br> Aanpassen van rol: <b>" .$selectRoleName. "<br>";
-//					echo "<input type='radio' value='aanpassen'> Selecteer als u wilt Aanpassen";
 					echo "<table>";
 					echo "<th>Status</th>";
 					echo "<th>Permission</th>";
@@ -73,87 +67,38 @@ $query = Database::getInstance()->query(
 						echo "<tr>";
 
 						?>
-                        <!--todo zorgen dat ze geselecteerd zijn als ze de permission hebben in de lijst user_permission_lists-->
-                        <td><input type="checkbox" name="checkbox[]" value"<?php echo $pid->id ?> <?php if ($pid->user_permission_id !== null) echo "checked='checked'"; ?>
-                            ></td>
+                        <td><input type="checkbox" name="checkbox[]" value="<?php echo $pid->id ?>" <?php if ($pid->user_permission_id !== null) echo "checked='checked'"; ?>></td>
 						<?php
-
-//                        echo $p->user_permission_id;
-						echo "<td>".$pid->user_permission_name ." </td>";
-						echo "<td>".$pid->user_permission_description. "</td>";
-
+                        echo "<td>".$pid->user_permission_name ." </td>";
+						 echo "<td>".$pid->user_permission_description. "</td>";
 					}
 				}
 				echo "</tr>";
 				echo "</table>";
-
-
-
-
-
-
-				//					VERWIJDEREN MOGELIJK geen van deze werkt de middelste stuurt het wel en de onderste ook maar meer niet
-				//				if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-				//					if(isset($_POST['aanpassen'])){
-				//						$checkboxes = isset( $_POST['checkbox'] ) ? $_POST['checkbox'] : array();
-				//						foreach ( $_POST['checkbox'] as $value ) {
-				//							Database::getInstance()->insert(
-				//								'user_permission_lists',
-				//								[
-				//									'role_name'          => $role_name,
-				//									'user_permission_id' => 3
-				//								]
-				//							);
-				//						}
-				//					} else{
-				//						echo "aanpassen niet gezet";
-				//					}
-				//				} else {
-				//					echo "geen post";
-				//				}
-
 				if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-					$checkboxes = isset( $_POST['checkbox'] ) ? $_POST['checkbox'] : array();
-					foreach ( $_POST['checkbox'] as $value ) {
-						Database::getInstance()->insert(
-							'user_permission_lists',
-							[
-								'role_name'          => "hii",
-								'user_permission_id' => 4
-							]
-						);
-					}
-				} else {
-					echo "probleem";
-				}
-
-
-
-
-
-
+				    if(isset($_POST['checkbox'])){
+					    $checkboxes = isset( $_POST['checkbox'] ) ? $_POST['checkbox'] : array();
+					    $roleName = $selectRoleName;
+					    Database::getInstance()->query(
+						    "DELETE FROM user_permission_lists WHERE role_name = '$selectRoleName'"
+					    );
+					    foreach ( $_POST['checkbox'] as $value ) {
+						    Database::getInstance()->insert(
+							    'user_permission_lists',
+							    [
+								    'role_name'          => $roleName,
+								    'user_permission_id' => $value
+							    ]
+						    );
+					    }
+                    }
+                }
 				?>
+                <!--TODO zorgen dat hij refreshed nadat hij is aangepast (1 x) -->
 
         </div>
-        <input type="hidden" name="token" value="<?php echo token::generate();?>">
         <input type="submit" value="Aanpassen">
         </form>
-		<?php
-		//        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		//	        $checkboxes = isset( $_POST['checkbox'] ) ? $_POST['checkbox'] : array();
-		//	        foreach ( $_POST['checkbox'] as $value ) {
-		//		        Database::getInstance()->insert(
-		//			        'user_permission_lists',
-		//			        [
-		//				        'role_name'          => "hii",
-		//				        'user_permission_id' => 2
-		//			        ]
-		//		        );
-		//	        }
-		//        } else {
-		//	        echo "probleem";
-		//        }
-		?>
         <br>
     </div>
 </div>
