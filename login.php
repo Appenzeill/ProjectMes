ea15be1155e9a01c54e7e7647c5116f6b7b3a18a
Jonathan van der Maas <jonathan.van.der.maas@in4datascience.com>
2019-04-18 16:36:46 +0200

    login uiterlijk veranderd

--- login.php
+++ login.php
@@ -55,31 +55,34 @@ if (!$user->count()) {
 		}
 	}
 ?>
+
 <body>
-<div class="login container d-flex h-75">
-    <div class="login-screen row justify-content-center align-self-center bg-dark">
-        <div class="app-title">
-            <h1 class="text-white mb-3">login</h1>
-        </div>
-
-        <form action="" method="post">
-
-        <div class="login-form">
-            <div class="form-group">
-                <input type="email" class="form-control bg-secondary text-white" name="email" data-validate="required email" id="login-name" placeholder="user@example.com" value="<?php echo Input::get('email')?>" />
-                <label class="login-field-icon fui-user" for="login-name"></label>
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="card card-signin my-5">
+        <div class="card-body">
+          <img class="mx-auto d-block mb-4" style="height: 100px;" src="images/trusted-p.svg" alt="">
+          <h1 class="text-center font-weight-bold">Trusted</h1>
+          <hr class="my-4">
+          <form class="form-signin" action="" method="post">
+            <div class="form-label-group">
+              <input type="email" class="form-control" name="email" data-validate="required email" id="login-name" placeholder="user@example.com" value="<?php echo Input::get('email')?>" />
+              <label for="login-name">Email address</label>
             </div>
 
-            <div class="form-group">
-                <input type="password" class="form-control bg-secondary text-white" data-validate="required" name="password" placeholder="wachtwoord" id="login-pass" id="login-pass"/>
-                <label class="login-field-icon fui-lock" for="login-pass"></label>
-            </div>
+              <div class="form-label-group">
+                <input type="password" class="form-control" data-validate="required" name="password" placeholder="wachtwoord" id="login-pass"/>
+                <label for="login-pass">Password</label>
+              </div>
+            <hr class="my-4">
             <?php echo $error;?>
             <input type="hidden" name="token" value="<?php echo Token::generate()?>">
-            <input class="btn btn-brand" type="submit" name="submit"  value="Login" />
+            <input style="background-color: #4c0e57" class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="submit" value="Login">
+          </form>
         </div>
+      </div>
     </div>
-    </form>
-
+  </div>
 </div>
 </body>
