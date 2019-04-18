fdb60a546d856de434cb1249295522190682c043
Jonathan van der Maas <jonathan.van.der.maas@in4datascience.com>
2019-04-18 20:59:51 +0200

    Added dropdown

--- Includes/parts/roleEdit.php
+++ Includes/parts/roleEdit.php
@@ -5,24 +5,24 @@ $nope = "";
 $id = Session::get(Config::get('session/session_name'));
 
 $user = Database::getInstance()->get(
-	'users',
-	[
-		'id', '=', $id
-	]);
+    'users',
+    [
+        'id', '=', $id
+    ]);
 foreach ($user->results() as $user) {
 }
 
-$permission_name_filler    = "";
-$permission_description_filler   = "";
+$permission_name_filler = "";
+$permission_description_filler = "";
 if (Input::exists()) {
-	$permission_name_filler         = Input::get('permission_name');
-	$permission_description_filler  = Input::get('permission_description');
+    $permission_name_filler = Input::get('permission_name');
+    $permission_description_filler = Input::get('permission_description');
 }
-$selectRoleName     = "";
-$selectRoleID       = "";
+$selectRoleName = "";
+$selectRoleID = "";
 
 if (Input::exists()) {
-	$selectRoleName     = Input::get('selectRole');
+    $selectRoleName = Input::get('selectRole');
 }
 
 
@@ -30,85 +30,88 @@ if (Input::exists()) {
  * Rollen selecteren:
  */
 $query = Database::getInstance()->query(
-	"SELECT DISTINCT role_name FROM user_permission_lists");
+    "SELECT DISTINCT role_name FROM user_permission_lists");
 ?>
 
 <div class="col-md-4 content">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3>Aanpassen van rechten</h3>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3>Aanpassen van rechten</h3>
+    </div>
+    <div class="panel-body">
+      <p>Selecteer de rol die u wilt aanpassen:</p>
+      <form action="" method="post">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">Options</label>
+          </div>
+          <select class="custom-select" id="inputGroupSelect01" name="selectRole">
+            <option selected>Choose...</option>
+              <?php
+              foreach ($query->results() as $query) {
+                  echo "<option " . (isset($_POST["selectRole"]) && $query->role_name == $_POST['selectRole'] ? "selected" : "") . " value='$query->role_name' ";
+                  echo "> " . $query->role_name . "</option>";
+              }
+              ?>
+          </select>
         </div>
-        <div class="panel-body">
-            <p>Selecteer de rol die u wilt aanpassen:</p>
-            <form action="" method="post">
-              <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                  <label class="input-group-text" for="inputGroupSelect01">Options</label>
-                </div>
-                <select class="custom-select" id="inputGroupSelect01" name="selectRole">
-                  <option selected>Choose...</option>
-                    <?php
-                    foreach ($query->results() as $query) {
-                        echo "<option " . (isset($_POST["selectRole"]) && $query->role_name == $_POST['selectRole'] ? "selected" : "" ) . " value='$query->role_name' "; echo "> ".$query->role_name."</option>";
-                    }
-                    ?>
-                </select>
-              </div>
-				<?php
-				$rawpermissions = Database::getInstance()->query(
-					"SELECT * FROM user_permission_lists WHERE role_name = '$selectRoleName'");
+          <?php
+          $rawpermissions = Database::getInstance()->query(
+              "SELECT * FROM user_permission_lists WHERE role_name = '$selectRoleName'");
 
-				if (!$rawpermissions->count()) {
-					echo "<br>Geen rol geselecteerd<br>";
-				} else {
-					$permissions = Database::getInstance()->query(
-						"SELECT  user_permissions.*, filtered_list.id as lists_id, filtered_list.role_name, filtered_list.user_permission_id FROM user_permissions LEFT JOIN (SELECT * FROM user_permission_lists WHERE user_permission_lists.role_name =  '$selectRoleName') filtered_list
+          if (!$rawpermissions->count()) {
+              echo "<br>Geen rol geselecteerd<br>";
+          } else {
+              $permissions = Database::getInstance()->query(
+                  "SELECT  user_permissions.*, filtered_list.id as lists_id, filtered_list.role_name, filtered_list.user_permission_id FROM user_permissions LEFT JOIN (SELECT * FROM user_permission_lists WHERE user_permission_lists.role_name =  '$selectRoleName') filtered_list
                                 ON user_permissions.id = filtered_list.user_permission_id
                                 ORDER BY user_permissions.id;");
-					echo "<br> Aanpassen van rol: <b>" .$selectRoleName. "<br>";
-					echo "<table>";
-					echo "<th>Status</th>";
-					echo "<th>Permission</th>";
-					echo "<th>Beschrijving</th>";
-					foreach ($permissions->results() as $pid) {
-						echo "<tr>";
+              echo "<br> Aanpassen van rol: <b>" . $selectRoleName . "<br>";
+              echo "<table>";
+              echo "<th>Status</th>";
+              echo "<th>Permission</th>";
+              echo "<th>Beschrijving</th>";
+              foreach ($permissions->results() as $pid) {
+                  echo "<tr>";
 
-						?>
-                        <td><input type="checkbox" name="checkbox[]" value="<?php echo $pid->id ?>" <?php if ($pid->user_permission_id !== null) echo "checked='checked'"; ?>></td>
-						<?php
-                        echo "<td>".$pid->user_permission_name ." </td>";
-						 echo "<td>".$pid->user_permission_description. "</td>";
-					}
-				}
-				echo "</tr>";
-				echo "</table>";
-				if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-				    if(isset($_POST['checkbox'])){
-					    $checkboxes = isset( $_POST['checkbox'] ) ? $_POST['checkbox'] : array();
-					    $roleName = $selectRoleName;
-					    Database::getInstance()->query(
-						    "DELETE FROM user_permission_lists WHERE role_name = '$selectRoleName'"
-					    );
-					    foreach ( $_POST['checkbox'] as $value ) {
-						    Database::getInstance()->insert(
-							    'user_permission_lists',
-							    [
-								    'role_name'          => $roleName,
-								    'user_permission_id' => $value
-							    ]
-						    );
-					    }
-					    echo "
+                  ?>
+                <td><input type="checkbox" name="checkbox[]"
+                           value="<?php echo $pid->id ?>" <?php if ($pid->user_permission_id !== null) echo "checked='checked'"; ?>>
+                </td>
+                  <?php
+                  echo "<td>" . $pid->user_permission_name . " </td>";
+                  echo "<td>" . $pid->user_permission_description . "</td>";
+              }
+          }
+          echo "</tr>";
+          echo "</table>";
+          if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+              if (isset($_POST['checkbox'])) {
+                  $checkboxes = isset($_POST['checkbox']) ? $_POST['checkbox'] : array();
+                  $roleName = $selectRoleName;
+                  Database::getInstance()->query(
+                      "DELETE FROM user_permission_lists WHERE role_name = '$selectRoleName'"
+                  );
+                  foreach ($_POST['checkbox'] as $value) {
+                      Database::getInstance()->insert(
+                          'user_permission_lists',
+                          [
+                              'role_name' => $roleName,
+                              'user_permission_id' => $value
+                          ]
+                      );
+                  }
+                  echo "
 					    <script>
-    window.location.replace(\"http://127.0.0.1/Trusted/?rolename_expand=\");
-</script>
+                  window.location.replace(\"http://127.0.0.1/Trusted/?rolename_expand=\");
+              </script>
 					    ";
-                    }
-                }
-				?>
-        </div>
-        <input type="submit" value="Aanpassen">
-        </form>
-        <br>
+              }
+          }
+          ?>
     </div>
+    <input type="submit" value="Aanpassen">
+    </form>
+    <br>
+  </div>
 </div>
