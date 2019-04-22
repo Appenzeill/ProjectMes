7af10c7d1204be440d0d41b8a75fb855c54dfc31
Appenzeill <Appenzellsniper@gmail.com>
2019-04-22 23:58:03 +0200

    Nieuwe layout, edit moet nog gefixed worden.

--- Includes/html-parts/html/top.php
+++ Includes/html-parts/html/top.php
@@ -3,20 +3,20 @@
 <html lang="en">
 
 <head>
-	<script src="vendor/jquery/jquery.min.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="">
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<title>Simple Sidebar - Start Bootstrap Template</title>
+    <title>Simple Sidebar - Start Bootstrap Template</title>
 
-	<!-- Bootstrap core CSS -->
-	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-	<!-- Custom styles for this template -->
-    <link href="CSS/main.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="CSS/simple-sidebar.css" rel="stylesheet">
 
 </head>
 
